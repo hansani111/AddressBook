71782f1e100f67577d8cea5bfd95cc16c3c580d9
hansani111 <hansanideshmukh894@gmail.com>
2023-01-13 18:32:24 +0530

    added: create a contact

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        AddressBook addressbook1 = new AddressBook();
+        addressbook1.addContactDetails();
+    }
+}
