622b5dabc5469bf01187e56a24e79236531aa273
hansani111 <hansanideshmukh894@gmail.com>
2023-01-13 18:40:44 +0530

    added: added existing contact

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -4,26 +4,29 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 
-        static Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
-        public static void main(String[] args) {
-            AddressBook adresssbook1 = new AddressBook();
-            boolean flag = true;
+    public static void main(String[] args) {
+        AddressBook addressbook1 = new AddressBook();
+        boolean flag = true;
 
-            do {
-                System.out.println("PRESS 1 to Add Contact");
-                int choice = sc.nextInt();
+        do {
+            System.out.println("PRESS 1 to Add Contact   ||   PRESS 2 to Edit Contact");
+            int choice = sc.nextInt();
 
-                switch (choice) {
-                    case 1:
-                        adresssbook1.addContactDetails();
-                        break;
-                    default:
-                        System.out.println("Invalid Input!!!! Please try again.");
-                        break;
-                }
-                System.out.println("\n*** Total Contacts present : " + adresssbook1.addressbook.size() + " ***");
-                System.out.println("\n");
-            } while (flag);
-        }
+            switch (choice) {
+                case 1:
+                    addressbook1.addContactDetails();
+                    break;
+                case 2:
+                    addressbook1.editContactDetails();
+                    break;
+                default:
+                    System.out.println("Invalid Input!!!! Please try again.");
+                    break;
+            }
+            System.out.println("\n*** Total Contacts : " + addressbook1.addressbook.size() + " ***");
+            System.out.println("\n");
+        } while (flag);
+    }
 }
