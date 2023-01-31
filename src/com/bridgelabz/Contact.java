6a3b2c12f6ab39cbbe4026173c7a446fdf5a1434
hansani111 <hansanideshmukh894@gmail.com>
2023-01-31 21:46:05 +0530

    added: no duplicate entry

--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -15,6 +15,16 @@ public class Contact {
 
     }
 
+    public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 
     public String getFirstName() {
         return firstName;
